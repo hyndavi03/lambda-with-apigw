bcbd8ea81576380fcb4dea8baf64ddd946ba1fa8
Hyndavi <venkatahyndavi@gmail.com>
2023-11-01 17:37:36 +0530

    updated

--- main.tf
+++ main.tf
@@ -87,18 +87,6 @@ resource "aws_apigatewayv2_stage" "example" {
   deployment_id = aws_apigatewayv2_deployment.example.id
 }
 
-output "api_gateway_url" {
-  value = aws_apigatewayv2_api.example.api_endpoint
-}
-
-output "lambda_function_arn" {
-  value = aws_lambda_function.example.arn
-}
-
-output "api_endpoint" {
-  value = aws_apigatewayv2_api.example.api_endpoint
-}
-
 
 terraform {
   required_version = ">= 1.0.0"
--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,3 @@
-output "vpc_id" {
-  value = aws_vpc.example.id
-}
-
-output "public_subnet_id" {
-  value = aws_subnet.public.id
-}
-
-output "private_subnet_id" {
-  value = aws_subnet.private.id
-}
-
-output "nat_gateway_id" {
-  value = aws_nat_gateway.example.id
-}
-
-
 output "api_gateway_url" {
   value = aws_apigatewayv2_api.example.api_endpoint
 }
@@ -26,4 +9,3 @@ output "lambda_function_arn" {
 output "api_endpoint" {
   value = aws_apigatewayv2_api.example.api_endpoint
 }
-
