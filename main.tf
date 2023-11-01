ffa539301c7ac1fc8c2d4c5baa3d9cd51171ed4d
Hyndavi <venkatahyndavi@gmail.com>
2023-11-01 14:05:50 +0530

    commit

--- main.tf
+++ main.tf
@@ -118,15 +118,16 @@ resource "aws_apigatewayv2_integration" "example" {
   integration_method = "POST"
 }
 
-resource "aws_apigatewayv2_route" "example" {
-  api_id    = aws_apigatewayv2_api.example.id
-  route_key = "$default"
-  target    = "integrations/${aws_apigatewayv2_integration.example.id}"
-}
-
 resource "aws_apigatewayv2_stage" "example" {
   api_id = aws_apigatewayv2_api.example.id
   name   = "test"
+}
+
+resource "aws_apigatewayv2_stage" "example" {
+  api_id          = aws_apigatewayv2_api.example.id
+  name            = "test"
+  auto_deploy     = true
+  deployment_id   = aws_apigatewayv2_deployment.example.id
 }
 
 resource "aws_apigatewayv2_route" "example_route" {
